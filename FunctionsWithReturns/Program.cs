0efb3533017324de863eef7470c3ab1652c08288
Opilane TTHK <opilane@tthk.ee>
2021-10-26 13:35:54 +0300

    EpicFightv2

---
+++ FunctionsWithReturns/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace FunctionsWithReturns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hero, villain;
+            string heroWeapon, villainWeapon;
+            hero = RandomHero();
+            heroWeapon = RandomWeapon();
+            villain = RandomVillain();
+            villainWeapon = RandomWeapon();
+
+            int heroHP = GenerateHP(hero);
+            int villainHP = GenerateHP(villain);
+
+            Console.WriteLine($"{hero} [{heroHP} HP] will fight {villain} [{villainHP} HP]");
+            Console.WriteLine($"{hero} will fight with {heroWeapon}");
+            Console.WriteLine($"{villain} will fight with {villainWeapon}");
+
+            while(heroHP > 0 && villainHP > 0)
+            {
+                heroHP = heroHP - Hit(villain, hero, villainWeapon);
+                villainHP = villainHP - Hit(hero, villain, heroWeapon);
+            }
+
+            if(heroHP <= 0)
+            {
+                Console.WriteLine("Villain WINS!");
+            }
+            else
+            {
+                Console.WriteLine("Hero saves the day!");
+            }
+        }
+
+        private static int Hit(string characterOne, string characterTwo, string weapon)
+        {
+            Random rnd = new Random();
+            int strike = rnd.Next(0, weapon.Length / 2);
+            Console.WriteLine($"{characterOne} hit {characterTwo} for {strike} HP!");
+
+            if(strike == weapon.Length / 2 - 1)
+            {
+                Console.WriteLine($"Awesome! {characterOne} made a critical hit!");
+            }
+            else if (strike == 0)
+            {
+                Console.WriteLine($"{characterTwo} dodged the attack!");
+            }
+
+            return strike;
+        }
+
+        private static int GenerateHP(string someCharacter)
+        {
+            Random rnd = new Random();
+            return rnd.Next(someCharacter.Length, someCharacter.Length + 10);
+        }
+
+        private static int RandomIndex(string[] someArray)
+        {
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, someArray.Length);
+
+            return randomIndex;
+        }
+
+        private static string RandomHero()
+        {
+            string[] heroes = { "Captain Underpants", "Iron Man", "Spiderman", "Thor", "Doctor Strange", "Hulk" };
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, heroes.Length);
+
+            return heroes[RandomIndex(heroes)];
+        }
+
+        private static string RandomVillain()
+        {
+            string[] villains = { "Venom", "Carnage", "Thanos", "Joker", "Bane", "Darth Vader" };
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, villains.Length);
+
+            string randomVillain = villains[randomIndex];
+            return villains[RandomIndex(villains)];
+        }
+
+        private static string RandomWeapon()
+        {
+            string[] weapon = { "Frying Pan", "Keyboard", "Mouse", "Butter knife", "Topelt juustuburger", "Zinger tower burger (spicy)", "McDonalds sprite", "McDonalds Ice Cream machine" };
+
+            return weapon[RandomIndex(weapon)];
+        }
+
+    }
+}
